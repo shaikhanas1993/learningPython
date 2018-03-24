d5f9acad37fef0e94fada69c70fb6ad3def26a4f
anas1993 <shaikhanas1993@gmail.com>
2018-03-24 12:21:51 +0530

    learned control flow of python

---
+++ comments.py
@@ -0,0 +1,6 @@
+#this is a first comment
+spam = 1 #this is a second comment
+    #this is third commet
+str = "#this is not a comment"
+
+print str
---
+++ second.py
@@ -0,0 +1,10 @@
+x = int(raw_input("Please provide me with a number::"))
+if x>0:
+    print "Found a Positive number"
+elif x<0:
+    print "Found a negative number"
+else:
+    print "Something weird you provided"
+
+
+

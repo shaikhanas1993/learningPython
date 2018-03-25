0fae5a58e3ba9d6341b373f541459cd79b30fb79
anas1993 <shaikhanas1993@gmail.com>
2018-03-25 18:44:43 +0530

    learned python function definition

---
+++ def.py
@@ -0,0 +1,82 @@
+
+
+
+# def fib(n):
+#     result = []
+#     a,b = 0,1
+#     while a<n:
+#         print a
+#         result.append(a)
+#         a,b = b,a+b
+#     return result
+
+# print fib(1000)
+
+
+
+
+
+
+# """Learning Functions in Python"""
+# def fib(n):
+#     result = []
+#     a,b = 0,1
+#     while a<n:
+#         a = 0
+
+        
+   
+
+# fib(100)
+
+
+# def fib(n):
+#     result = []
+#     a,b = 0,1
+#     while a<n:
+#         result.append(a)
+#         a,b = b,a+b
+#     return result    
+# print fib(100)
+
+
+#print f    
+
+
+
+# def fib(n):
+#     """ Function to print fibonacci series"""
+#     a,b = 0,1
+#     while a<n:
+#         print a
+#         a,b = b,a+b
+
+# fib(100)
+
+# n=100
+
+# def fib(n):
+#     n = 20
+#     print n
+
+# fib(n)
+
+# def fib1(a):
+#     print "fib1",a
+#     fib(a=35)
+
+
+# def fib(a):
+#     print "fib",a
+#     a =  20
+#     fib1(a)
+#     print "After call",a
+
+# # print n
+# a = 10
+# fib(a)
+
+
+
+
+# print "a==",a

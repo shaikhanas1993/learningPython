979cd1937274805166f6ef74421cc044e97378e9
anas1993 <shaikhanas1993@gmail.com>
2018-03-25 11:35:00 +0530

    learned about pass statements

---
+++ pass.py
@@ -0,0 +1,16 @@
+#pass statement does nothing 
+#pass statment is used then i need a syntax to keep myprogram up and running
+#but allowing to keep it at abstract level
+
+class emptyClass:
+    pass
+
+    # def test():
+    #     pass
+
+
+
+# def test:
+#     pass
+
+

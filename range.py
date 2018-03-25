298c3452ba2ab5cbce3d6b3b80b959eaee6938e9
anas1993 <shaikhanas1993@gmail.com>
2018-03-25 10:04:59 +0530

    range function

---
+++ range.py
@@ -0,0 +1,42 @@
+#Going Deep Inside range
+
+"""
+    Range Function expects an integer.
+    If Not Provided gives an error.
+    See Stack Trace:
+     integer  argument expected
+"""
+
+print "========================"
+print range(10)
+print "========================"
+
+print range(5,10)
+print "========================"
+
+print range(5,10,3)
+print "========================"
+
+print range(0,-10,-3)
+
+
+
+# myList = ['mary','kom','movuew']
+
+# for item in range(len(myList)):
+#     print item
+#     print myList[item]
+
+
+myList  = ['this','us','a','tst']
+for index in range(len(myList)):
+    print index
+    print myList[index]
+
+
+
+
+
+
+
+

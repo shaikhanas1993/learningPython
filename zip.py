b6e329b84fb5bfcc4d9c8c7cb1fcff19c888982f
anas1993 <shaikhanas1993@gmail.com>
2018-04-01 13:13:47 +0530

    learned about zip and arguments unpacking

---
+++ zip.py
@@ -0,0 +1,30 @@
+# name = [ "Manjeet", "Nikhil", "Shambhavi", "Astha" ]
+# roll_no = [ 4, 1, 3, 2 ]
+# marks = [ 40, 50, 60, 70 ]
+# mapped = zip(name, roll_no, marks)
+# mainList = []
+# for tup in zip(*mapped):
+#     mainList.append(list(tup))
+
+# print   mainList  
+# args = [3, 6]
+# for i in range(*args):
+#     print i
+
+
+# name = ['manjeet','astha','renuka']
+# roll_no = [44,28,59]
+# marks = [12,56,97]
+# zop =  zip(name,roll_no,marks)
+# mySet = set(zop)
+# print mySet
+
+# set
+
+
+mx = [
+    [1,2],
+    [3,4]
+]
+
+print zip(*mx)

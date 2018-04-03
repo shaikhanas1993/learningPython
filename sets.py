5fdbd11b41442881c14a2b1749a0e46d53927b96
anas1993 <shaikhanas1993@gmail.com>
2018-04-03 23:54:47 +0530

    learned about set operations

--- sets.py
+++ sets.py
@@ -1,6 +1,17 @@
 mylist = ['apple','apple','banana']
-set_of_list = set(mylist)
+a = set(mylist)
 #converting set to list to retrieve value
-back_to_list = list(set_of_list)
-del set_of_list
-print back_to_list[0]
+# back_to_list = list(set_of_list)
+# del set_of_list
+# print back_to_list[0]
+
+b = set(['jason','banana'])
+print a - b #there in a but not in b 
+print b-a # remove everything  a has,leaving only b's element that are not present in a
+
+print a & b # intersection that is both a & b have
+
+print a | b # union operation
+
+print a ^ b #remove common elements between a & b
+

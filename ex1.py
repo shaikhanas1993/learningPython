a22714c50c0080bd45ebd9fbd9dc5e6ee94e0bba
anas1993 <shaikhanas1993@gmail.com>
2018-04-14 12:21:27 +0530

    more exceptions

--- ex1.py
+++ ex1.py
@@ -1,11 +1,26 @@
-import sys
-for arg in sys.argv[1:]:
-    print arg
-    try:
-        f = open(arg,'r')
-    except IOError as e:
-        print "Cannot Open",e
-    else:
-        print f.read()
-        # f.close()
+# import sys
+# for arg in sys.argv[1:]:
+#     print arg
+#     try:
+#         f = open(arg,'r')
+#     except IOError as e:
+#         print "Cannot Open",e
+#     else:
+#         print f.read()
+#         # f.close()
     
+# try:
+#     x = 1/1
+#     # print x
+# except ZeroDivisionError as e:
+#     print e
+# else:
+#     print x
+
+
+try:
+    raise Exception('spam','eggs')
+except Exception as inst:
+    print type(inst.args)
+    print inst.args[0]
+
